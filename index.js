134fc705e07d2076bd7b450c8350e3dd6897f0bb
Bolat <bolatah@.outlook.com>
2021-10-26 12:00:29 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const ratelimit = require('express-rate-limit');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// Rate limiting
+
+const limiter = ratelimit({
+    windowsMs: 10*60*1000, // 10 Mins
+    max: 5 
+});
+app.use(limiter);
+app.set('trust proxy', 1)
+
+// Set static folder
+app.use(express.static('public')); 
+
+// Routes
+app.use('/api', require('./routes'));
+
+// enable cors
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server run port ${PORT}`));
